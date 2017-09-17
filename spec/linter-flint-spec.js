5b3be15c0192f9563c79e6cb2011a12b8795679b
Landon Abney <Landonabney@gmail.com>
2017-09-17 16:46:14 -0700

    Asyncify the specs
    Bring in `jasmine-fix` to allow the use of `async`/`await` in the specs
    and refactor them to take advantage of this.

--- spec/linter-flint-spec.js
+++ spec/linter-flint-spec.js
@@ -1,5 +1,7 @@
 'use babel';
 
+// eslint-disable-next-line no-unused-vars
+import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 import * as path from 'path';
 
 const { lint } = require('../lib/linter-flint.js').provideLinter();
@@ -8,50 +10,50 @@ const goodPath = path.join(__dirname, 'fixtures', 'goodProj', 'package.json');
 const badPath = path.join(__dirname, 'fixtures', 'badProj', 'package.json');
 
 describe('The Flint provider for Linter', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     // Close any open file
     atom.workspace.destroyActivePaneItem();
     // Close all project paths
     const projectPaths = atom.project.getPaths();
     projectPaths.forEach(projectPath => atom.project.removePath(projectPath));
     // Activate the linter
-    waitsForPromise(() => atom.packages.activatePackage('linter-flint'));
+    await atom.packages.activatePackage('linter-flint');
   });
 
-  it('checks a project with no issues and finds nothing wrong', () => {
+  it('checks a project with no issues and finds nothing wrong', async () => {
     atom.project.addPath(path.dirname(goodPath));
-    waitsForPromise(() => atom.workspace.open(goodPath).then(editor =>
-      lint(editor).then(messages =>
-        expect(messages.length).toBe(0))));
+    const editor = await atom.workspace.open(goodPath);
+    const messages = await lint(editor);
+    expect(messages.length).toBe(0);
   });
 
-  it('checks a project with issues and reports the found issues', () => {
+  it('checks a project with issues and reports the found issues', async () => {
     atom.project.addPath(path.dirname(badPath));
-    waitsForPromise(() => atom.workspace.open(badPath).then(editor =>
-      lint(editor).then((messages) => {
-        expect(messages[0].type).toBe('Warning');
-        expect(messages[0].severity).toBe('warning');
-        expect(messages[0].html).not.toBeDefined();
-        expect(messages[0].text).toBe('Bootstrap script not found');
-        expect(messages[0].filePath).not.toBeDefined();
-        expect(messages[0].range).not.toBeDefined();
-        expect(messages[0].trace).toEqual([{
-          type: 'Trace',
-          severity: 'info',
-          html: 'A bootstrap script makes setup a snap. (<a href="http://bit.ly/JZjVL6">link</a>)',
-        }]);
+    const editor = await atom.workspace.open(badPath);
+    const messages = await lint(editor);
 
-        expect(messages[1].type).toBe('Warning');
-        expect(messages[1].severity).toBe('warning');
-        expect(messages[1].html).not.toBeDefined();
-        expect(messages[1].text).toBe('Test script not found');
-        expect(messages[1].filePath).not.toBeDefined();
-        expect(messages[1].range).not.toBeDefined();
-        expect(messages[1].trace).toEqual([{
-          type: 'Trace',
-          severity: 'info',
-          html: 'Make it easy to run the test suite regardless of project type. (<a href="http://bit.ly/JZjVL6">link</a>)',
-        }]);
-      })));
+    expect(messages[0].type).toBe('Warning');
+    expect(messages[0].severity).toBe('warning');
+    expect(messages[0].html).not.toBeDefined();
+    expect(messages[0].text).toBe('Bootstrap script not found');
+    expect(messages[0].filePath).not.toBeDefined();
+    expect(messages[0].range).not.toBeDefined();
+    expect(messages[0].trace).toEqual([{
+      type: 'Trace',
+      severity: 'info',
+      html: 'A bootstrap script makes setup a snap. (<a href="http://bit.ly/JZjVL6">link</a>)',
+    }]);
+
+    expect(messages[1].type).toBe('Warning');
+    expect(messages[1].severity).toBe('warning');
+    expect(messages[1].html).not.toBeDefined();
+    expect(messages[1].text).toBe('Test script not found');
+    expect(messages[1].filePath).not.toBeDefined();
+    expect(messages[1].range).not.toBeDefined();
+    expect(messages[1].trace).toEqual([{
+      type: 'Trace',
+      severity: 'info',
+      html: 'Make it easy to run the test suite regardless of project type. (<a href="http://bit.ly/JZjVL6">link</a>)',
+    }]);
   });
 });
